49c538ec177721cb163dddbfc9e5737ce70e8313
Mikolaj Adamus <36365454+Mikeos42@users.noreply.github.com>
2021-11-16 19:43:28 +0000

    License done

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main() {
   License a("Helmut", 500);
 
-  // std::cout << a.get_guildname() << std::endl;
-  // std::cout << a.get_salary() << std::endl;
+  std::cout << a.get_guildname() << std::endl;
+  std::cout << a.get_salary() << std::endl;
+
+  std::cout << a;
 } 
